7b1274cf416f3157bac5866b6d88aeae4c2ff4de
REagleAndres <87045450+REagleAndres@users.noreply.github.com>
2025-10-08 09:57:16 -0600

    Update Projecto.ino
    Cambios al codigo para el uso con arduino

--- Equipo5/src/Projecto.ino
+++ Equipo5/src/Projecto.ino
@@ -1,26 +1,44 @@
 #include <iostream>
 #include <cmath>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(5, 4, 3, 2, 1, 0)
+
 
 int buzzer = 0; //simulando el buzzer, borrar luego
 int motorSpeed = 0; //simulando el motor, borrar luego
 bool status;
-int blue = 0;
-int red = 0;
+const int pinBuzzer = 11;
+const int pinMotor = 9;
+const int pinGreen = 6;
+const int pinBlue = 7;
+const int pinRed = 8;
+const int pinPower = 10;
+const int pinPoten = A1;
+const int pinSensor = A0;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600)
 
   //Estado: apagado = false, encendido = true, iniciamos apagados, hay que presionar el boton para dar inicio
+  pinMode(pinPower, INPUT);
   status =  true;
 
   //Velocidad del motor
   motorSpeed = 0; //asumiendo que asi funciona el motor
+  pinMode(pinMotor, OUTPUT);
+
 
   //Datos del Led RGB 
-  
+  pinMode(pinGreen, OUTPUT);
+  pinMode(pinBlue, OUTPUT);
+  pinMode(pinRed, OUTPUT);
+
   //int green = 0; posiblemente si usamen el led rgb
   //Revisar datos de colores rgb
 
+  
 }
 
   //Colocar rango de temperatura minima y maxima default
@@ -35,16 +53,22 @@ void setup() {
   int counter = 2; //para uso con el buzzer, sujeto a cambios para optimizar
 
 void loop() {
-  counter > 4 ? counter = 4 : counter = counter;
-  counter < 0 ? counter = 0 : counter = counter;
   // put your main code here, to run repeatedly:
   //Leer el valor de temperatura deseable si hubo un cambio con el potenciometro
-  //por ahora no vamos a simular esto ya que se usan eventos para leer el potenciometro
+  valueInput = map(analogRead(pinPoten), 0, 1023,-15, 100);
+  if(valueInput != targetTemp){
+    targetTemp = valueInput;
+    //Actualizar la pantalla
+  }
 
   //encendido-apagado
+  attachInterrupt(digitalPinToInterrupt(pinPower), power, change); //Manda a la funcion power si el boton se toco  
   if (!status){ //esto deria terminar el loop() si no esta encendido el aparato
     return;
   }
+
+  counter > 4 ? counter = 4 : counter = counter;
+  counter < 0 ? counter = 0 : counter = counter;
 
   std::cout << "Temperatura: \n";
   int currentTemp;
@@ -75,6 +99,8 @@ void loop() {
   //  Darle sonido al buzzer
   //  Encender ventilador si llega a temperatura de limite a la velocidad requerida utilizando el motor
   buzzer = alarm[c]; //Posiblemente cambie para optimizacion
+  //Recordar darle delay al buzzer
+  tone(pinBuzzer, buzzer);
   switch (change)
   {
   case 1:
@@ -99,6 +125,11 @@ void loop() {
   std::cout << "Valor del buzzer: " << buzzer << "\n";
   std::cout << "Red: " << red << "\n";
   std::cout << "Blue: " << blue << "\n";
+}
+
+void power(){
+  status = !status;
+  return;
 }
 
 //* Notas: ciertos valores no funcionan adecuadamente simulandolos sin embargo si los vemos en la realidad debido a que la temperatura no sube discretamente 
